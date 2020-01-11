c8268477359b587089998f47df27f43fde60b404
vijaychoudhary3131 <vijaychoudhary3131@gmail.com>
2020-01-11 12:09:37 +0530

    modify

--- Spring_Bean_Lifecycle/src/com/check/Test.java
+++ Spring_Bean_Lifecycle/src/com/check/Test.java
@@ -1,3 +1,4 @@
+
 public static void main(String args[]){
 
 System.out.println("Arun");
@@ -7,11 +8,17 @@ System.out.println("Arun");
 
 
 
-
+System.out.println("vijay");
 
 System.out.println("rajesh morya");
 
 System.out.println("shashank");
 System.out.println("ronak");
+
+
+public static void main(String args[])
+{
+System.out.println("shashank");
+System.out.println("vijay")
 
 }
